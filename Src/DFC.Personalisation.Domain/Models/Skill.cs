5d8f27a64ce7451c8787087b9d41eea29e0f0619
Matt Daniels <matt@techtoniq.com>
2019-12-16 12:46:27 +0000

    Skeleton solution

---
+++ Src/DFC.Personalisation.Domain/Models/Skill.cs
@@ -0,0 +1,12 @@
+﻿namespace DFC.Personalisation.Domain.Models
+{
+    public class Skill
+    {
+        public string Name { get; private set; }
+
+        public Skill(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Tests/DFC.Personalisation.Domain.Tests/Models/SkillTests.cs
@@ -0,0 +1,18 @@
+﻿using DFC.Personalisation.Domain.Models;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DFC.Personalisation.Domain.Tests.Models
+{
+    [TestFixture]
+    public class SkillTests
+    {
+        [Test]
+        public void When_SkillCreated_Then_NameShouldHaveValue()
+        {
+            var s = new Skill("perform upholstery repair");
+
+            s.Name.Should().Be("perform upholstery repair");
+        }
+    }
+}
